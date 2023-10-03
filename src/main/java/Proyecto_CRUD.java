f1435427f201a3fcdcda59ddeecdf4f2a276fe94
GulloJoseLeandro <60196978+gullojoseleandro@users.noreply.github.com>
2023-10-03 11:47:05 -0300

    app funcional, falta retocar algunas cositas

---
+++ src/main/java/Proyecto_CRUD.java
@@ -0,0 +1,16 @@
+
+import IGU.PantallaPrincipal;
+
+
+
+public class Proyecto_CRUD {
+
+    public static void main(String[] args) {
+        PantallaPrincipal principal = new PantallaPrincipal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+        
+        
+    }
+}
